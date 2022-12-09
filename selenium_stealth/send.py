9901d0e28d2256848dd97eed1b2d480ab9f44e9e
nandorlengyel <47507366+nandorlengyel@users.noreply.github.com>
2022-12-09 12:19:11 +0100

    Move send

---
+++ selenium_stealth/send.py
@@ -0,0 +1,10 @@
+
+import json
+
+
+def send(driver, cmd, params={}):
+    resource = "/session/%s/chromium/send_command_and_get_result" % driver.session_id
+    url = driver.command_executor._url + resource
+    body = json.dumps({'cmd': cmd, 'params': params})
+    response = driver.command_executor._request('POST', url, body)
+    return response.get('value')
